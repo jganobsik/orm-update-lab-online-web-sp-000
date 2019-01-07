06d95448f22509a675ad2832c40f8a9a899a1e37
Joe Ganobsik <jganobsik@gmail.com>
2019-01-07 14:59:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,9 @@ def save
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end
 
-
+def create(name, grade)
+  new_student = student.new(name, grade)
+  new_student.save
+end
 
 end
