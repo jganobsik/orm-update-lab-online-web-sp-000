469dcd8237529eda6bdd2bcb06705689ec5c42e0
Joe Ganobsik <jganobsik@gmail.com>
2019-01-07 15:02:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,4 +47,13 @@ def self.create(name, grade)
   new_student.save
 end
 
+def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end
+
 end
