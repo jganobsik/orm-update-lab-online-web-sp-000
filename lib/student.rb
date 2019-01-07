8ab523ca247c1087f6412fa805cab831b2f772ab
Joe Ganobsik <jganobsik@gmail.com>
2019-01-07 15:16:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,10 @@ def self.drop_table
         
         DB[:conn].execute(sql) 
       end
-  def save    
+  def save   
+    if self.id != nil 
+      self.update
+    else
  sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
@@ -40,6 +43,7 @@ def self.drop_table
 
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
   end
 
 def self.create(name, grade)
